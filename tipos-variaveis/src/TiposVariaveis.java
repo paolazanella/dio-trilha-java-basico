6826f298afbfbe3d08080cb4d69acab24fcc0e72
Paola Zanella <pzanella@edu.univali.br>
2024-05-13 14:02:26 -0300

    nao lembro

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,14 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        
+        double salarioMinimo = 2.500;
+
+        short numeroCurto = 1;
+        int numeroNotmal = numeroCurto;
+        short numeroCurto2  = (short)numeroNotmal;
+        int numero = 5;
+
+        numero =10;
+        System.out.print(numero);
     }
 }
+ 
