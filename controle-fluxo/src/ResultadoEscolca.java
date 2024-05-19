46cbd1391251f3e2c1e22606cbb3047a6c585270
Paola Zanella <pzanella@edu.univali.br>
2024-05-19 18:07:30 -0300

    aula sobre condicional ternaria

--- controle-fluxo/src/ResultadoEscolca.java
+++ controle-fluxo/src/ResultadoEscolca.java
@@ -1,13 +1,8 @@
 public class ResultadoEscolca{
     
     public static void main(String[] args) {
-        int nota =10;
-        if(nota >=7){
-            System.out.println("Aprovado");
-        }else if (nota >=  5 &&  nota < 7){ // smepre tem que retorcar ou true ou false 
-            System.out.println("Prova Recuperacao");
-        }else {
-            System.out.println("Reprovado");
-        }
+        int nota = 3;
+        String resultado = nota >= 7 ? "Aprovado" : nota >=5 && nota <7? "Recuperacao":"Reprovado";
+        System.out.println(resultado);
     }
 }
