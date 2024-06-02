1340e4033bc1fec0988801d05650a54c9da5bcec
Paola Zanella <pzanella@edu.univali.br>
2024-06-02 16:31:21 -0300

    conteudo sobre analisando candidatos

---
+++ controle-candidatos/src/candidatura/ProcesoSeletivo.java
@@ -0,0 +1,22 @@
+package candidatura;
+
+public class ProcesoSeletivo {
+
+    public static void main(String[] args) {
+        analisarCandidato(1900.0);
+        analisarCandidato(2200.0);
+        analisarCandidato(2000.0);
+    }
+
+    static void analisarCandidato(double salarioPretendido) {
+        double salarioBase = 2000.0;
+        if (salarioBase > salarioPretendido) {
+            System.out.println("Ligar para o candidato");
+        } else if (salarioBase == salarioPretendido) {
+            System.out.println("Ligar para o canditado com contra proposta");
+        } else {
+            System.out.println("Agradando os resultados dos demis canditados");
+        }
+
+    }
+}
