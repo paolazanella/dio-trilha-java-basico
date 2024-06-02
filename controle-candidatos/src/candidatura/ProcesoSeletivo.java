beac10cc827fd0c05da3cd7112b1694cb5b823f7
Paola Zanella <pzanella@edu.univali.br>
2024-06-02 17:34:04 -0300

    aula sobre selecionadno candidatos

--- controle-candidatos/src/candidatura/ProcesoSeletivo.java
+++ controle-candidatos/src/candidatura/ProcesoSeletivo.java
@@ -1,11 +1,42 @@
 package candidatura;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcesoSeletivo {
 
     public static void main(String[] args) {
-        analisarCandidato(1900.0);
+        /* analisarCandidato(1900.0);
         analisarCandidato(2200.0);
-        analisarCandidato(2000.0);
+        analisarCandidato(2000.0); */
+
+        selecaoCandidato();
+
+    }
+
+    static void selecaoCandidato() {
+
+        String[] candidatos = {"Felipe", "Marcia", "Julia", "Pailo", "Ahusto", "Monica", "Fabricio", "Mirela", "DAniela", "Jorge"};
+
+        int candidatoSelecinados = 0;
+        int candidatoAtul = 0;
+        double salarioBase = 2000.0;
+        while (candidatoSelecinados < 5 && candidatoAtul < candidatos.length) {
+            String candidato = candidatos[candidatoAtul];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato" + candidato + " Solicitou este valor de salario " + salarioPretendido);
+            if (salarioBase >= salarioPretendido) {
+                System.out.println("O candidato" + candidato + " foi selecinado para a vaga");
+                candidatoSelecinados++;
+            }
+            candidatoAtul++;
+
+        }
+
+    }
+
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
     }
 
     static void analisarCandidato(double salarioPretendido) {
